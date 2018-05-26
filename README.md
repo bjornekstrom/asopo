26a505e38b5c8716a8f5d548c4e1a80bcb581c23
bjornekstrom <bjorn@bjornekstrom.se>
2018-05-26 15:18:47 +0200

    Asopo added

---
+++ README.md
@@ -0,0 +1,15 @@
+# 🍜 Asopo
+
+Script for scraping links from a website.
+
+## Prerequisites
+
+### BeautifulSoup
+
+`pip install beautifulsoup4`
+
+## Usage
+
+1. Open `asopo.py` in a text editor and insert a webpage link within `("")` in the parenthesis at line 5.
+2. `python asopo.py >> out.txt` prints `https` and `http` links to the file `out.txt`
+3. Bon appétit! 🍜
---
+++ asopo.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+
+html_page = urllib2.urlopen("https://news.ycombinator.com")
+
+asopo = BeautifulSoup(html_page, "html.parser")
+
+for link in asopo.findAll('a', attrs={'href': re.compile("^http://")}):
+    http = link.get('href')
+    
+    print http
+
+for link in asopo.findAll('a', attrs={'href': re.compile("^https://")}):
+    https = link.get('href')
+
+    print https
